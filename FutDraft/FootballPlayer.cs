80157a4d722df77bc38cc626d450c8760fa1239c
hugo <160583187+mrmanhugo@users.noreply.github.com>
2024-10-18 16:14:43 +0200

    footballplayer

---
+++ FutDraft/FootballPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace DefaultNamespace;
+
+public class FootballPlayer
+{
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string Position { get; set; }
+        public string Nation { get; set; }
+        public string Club { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Rating} Rating - {Position} - {Nation} - {Club}";
+        }
+    }
+}
